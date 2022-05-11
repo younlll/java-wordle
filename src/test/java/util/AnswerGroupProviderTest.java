0fc5c717ecec64e399ecf0617f0389cdd4e70076
younlll <youn.lee96@gmail.com>
2022-05-11 22:52:07 +0900

    test: add test displayname

--- src/test/java/util/AnswerGroupProviderTest.java
+++ src/test/java/util/AnswerGroupProviderTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import model.Characters;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AnswerGroupProviderTest {
 
     @Test
+    @DisplayName("words.txt파일에서 제공된 단어 전체를 불러왔는지 확인")
     void provide() {
         List<Characters> provide = AnswerGroupProvider.provide();
 
